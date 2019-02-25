d36ce624b8ed44b85fb94b3f1ffe471ad4354625
mikolunar <mikolunar@gmail.com>
2019-02-25 22:32:15 +0000

    changes to styling

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+from django.contrib import messages
 
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
 
 def register(request):
 
@@ -10,13 +14,17 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            
+            messages.success(request, f'User created!')
+
+        else:
+            messages.error(request, f'Error while creating user!')
+            render(request, 'registration/register.html', {'form': form})
 
     else:
         form = UserCreationForm()
     context = {
-        'form':form
+        'form': form
 
     }
 
-    return render (request, 'registration/register.html', context)
+    return render(request, 'registration/register.html', context)
